6ebbd4db4eda2050b042ff6249c022e6789ef4dc
moazelkahlawy <mezo.abdo65@gmail.com>
2022-05-01 04:07:34 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+import Header from "./common/header/Header";
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Pages from "./pages/Pages";
+import Cart from "./common/Cart/Cart";
+import Data from "./components/flashDeals/Data";
+import Sdata from "./components/shops/Sdata";
+import Footer from "./common/footer/Footer";
+function App() {
+  const { productItems } = Data;
+  const { shopItems } = Sdata
+  const [cartItem, setCartItem] = useState([]);
+
+  const addToCart = (product) => {
+    const productExit = cartItem.find((item) => item.id === product.id);
+    if (productExit) {
+      setCartItem(cartItem.map((item) =>item.id === product.id? { ...productExit, qty: productExit.qty + 1 }: item));
+    } else {
+      setCartItem([...cartItem,{...product,qty: 1,},]);
+    }
+  };
+  const decreaseQty = (product) => {
+    const productExit = cartItem.find((item) => item.id === product.id);
+    if (productExit.qty === 1) {
+      setCartItem(cartItem.filter((item) => item.id !== product.id));
+    } else {
+      setCartItem(cartItem.map((item) =>item.id === product.id? { ...productExit, qty: productExit.qty - 1 }: item));
+    }
+  };
+
+  return (
+    <>
+      <Router>
+        <Header cartItem={cartItem} />
+        <Switch>
+          <Route path="/" exact>
+            <Pages productItems={productItems} addToCart={addToCart} shopItems={shopItems}/>
+          </Route>
+          <Route path="/cart" exact>
+            <Cart cartItem={cartItem} addToCart={addToCart} decreaseQty={decreaseQty} />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Annocument from "../components/annocument/Annocument";
+import Discount from "../components/discount/Discount";
+import FlashDeals from "../components/flashDeals/FlashDeals";
+import Home from "../components/MainPage/Home";
+import NewArrivals from "../components/newarrivals/NewArrivals";
+import Shop from "../components/shops/Shop";
+import TopCate from "../components/top/TopCate";
+import Wrapper from "../components/wrapper/Wrapper";
+
+function Pages({ productItems, cartItem, addToCart, shopItems }) {
+  return (
+    <>
+      <Home cartItem={cartItem} />
+      <FlashDeals productItems={productItems} addToCart={addToCart} />
+      <TopCate />
+      <NewArrivals />
+      <Discount />
+      <Shop shopItems={shopItems} addToCart={addToCart} />
+      <Annocument />
+      <Wrapper />
+    </>
+  );
+}
+
+export default Pages;
